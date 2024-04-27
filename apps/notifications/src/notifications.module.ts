f467a0329ecdf0445f9849774c36a23d2d262fb9
PGabriel20 <bielporreca@gmail.com>
2024-04-27 16:21:44 -0300

    Notification service working, now receives user email

---
+++ apps/notifications/src/notifications.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi'
+import { LoggerModule, NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+    LoggerModule,
+  ],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
+})
+export class NotificationsModule {}
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,17 +1,22 @@
-import { CreateChargeDto } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from 'apps/payments/src/dto/payments-create-charge.dto';
 import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly configService: ConfigService){}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE) private readonly notificationsService: ClientProxy
+  ){}
 
   private readonly stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'), {
     apiVersion: '2024-04-10'
   })
 
-  async createCharge({card, amount}: CreateChargeDto) {
+  async createCharge({card, amount, email}: PaymentsCreateChargeDto) {
     //@TODO - figure out how to create token from  card info
     const paymentMethod = await this.stripe.paymentMethods.create({
       type: 'card',
@@ -27,6 +32,8 @@ export class PaymentsService {
       payment_method_types: ['card'],
       currency: 'usd'
     })
+
+    this.notificationsService.emit('notify_email', { email })
 
     return paymentIntent
   }
